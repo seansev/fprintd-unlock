471f4f55ea6e48eb88deaf45bef7fca5e0122e0e
Sean S. Williams <mail@seansev.com>
2026-05-08 16:02:14 -0400

    Begin parsing args

---
+++ main.c
@@ -0,0 +1,68 @@
+/*
+ * fprintd-unlock - Spawn a screen locker; unlock it with a valid fingerprint.
+ * Copyright (C) 2026 Sean S. Williams
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+*/
+
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "config.h"
+
+static struct option longopts[] = {
+        {"signal", required_argument, NULL, 's'},
+        {"help", no_argument, NULL, 'h'},
+        {0},
+};
+
+static void display_help()
+{
+        execlp("man", "man", PROGRAM_NAME, NULL);
+        /* exec failed */
+        printf("See `man %s'.\n", PROGRAM_NAME);
+        exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+        int c;
+        char *arg;
+
+        while ((c = getopt_long(argc, argv, "+s:h", longopts, NULL)) != -1) {
+                switch (c) {
+                case 's':
+                        arg = optarg;
+                        break;
+                case 'h':
+                        display_help();
+                        break;
+                case '?':
+                        return EXIT_FAILURE;
+                default:
+                        return EXIT_FAILURE;
+                }
+        }
+
+        printf("%s\n", arg);
+
+        return EXIT_SUCCESS;
+}
