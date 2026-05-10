d1218842e7b1457c42a495930edff64dfec33fc6
Sean S. Williams <mail@seansev.com>
2026-05-10 01:17:50 -0400

    Conditional compilation to include sd-bus

---
+++ include/dbus.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* Pattern borrowed from mako */
+#if defined(HAVE_LIBSYSTEMD)
+#include <systemd/sd-bus.h>
+#elif defined(HAVE_LIBELOGIND)
+#include <elogind/sd-bus.h>
+#elif defined(HAVE_BASU)
+#include <basu/sd-bus.h>
+#endif
